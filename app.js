4770d0c50e4d3eeef4bf4e8360ca1c7f1a886e8a
Bless <tetaangelbless014@gmail.com>
2025-10-28 16:06:47 +0200

    Initialised the frontend of this simple planning tool

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-let goal=document.getElementById('#goalsInput');
-const addBtn= document.getElementById('#addBtn')
+let goal=document.getElementById('goalsInput');
+let goalList=document.getElementById('goalList')
+const addBtn= document.getElementById('addBtn');
+
+let goals=[];
+
+addBtn.addEventListener(onclick,()=>{
+    const goalText = goal.value.trim();
+    if(goalText==="")return;
+    const newGoal=goals.push()
+})
