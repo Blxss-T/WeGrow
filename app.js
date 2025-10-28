769584e094c5453d5120f0571126c3c7333154c3
Bless <tetaangelbless014@gmail.com>
2025-10-28 16:13:13 +0200

    Initialised the frontend of this simple planning tool

--- app.js
+++ app.js
@@ -7,5 +7,15 @@ let goals=[];
 addBtn.addEventListener(onclick,()=>{
     const goalText = goal.value.trim();
     if(goalText==="")return;
-    const newGoal=goals.push()
+    const newGoal={
+        id:Date.now(),
+        title:goalText,
+        completed:false
+    };
+    goals.push(newGoal);
+    goal.value=" ";
+    renderGoals();
 })
+function renderGoals(){
+    goalList.innerHTML='';
+}
